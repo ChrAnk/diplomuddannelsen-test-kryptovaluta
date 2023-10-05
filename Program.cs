0874a09aa6c805e6eabeea63b63908dcacd2fe1f
ChrAnk <33005356+ChrAnk@users.noreply.github.com>
2023-10-05 21:46:50 +0200

    Updated

--- Program.cs
+++ Program.cs
@@ -3,16 +3,13 @@
 
 	Dictionary<string, double> currencyPriceList = new() { };
 
-    /// <summary>
-    /// Angiver prisen for en enhed af en kryptovaluta. Prisen angives i dollars.
-    /// Hvis der tidligere er angivet en værdi for samme kryptovaluta, 
-    /// bliver den gamle værdi overskrevet af den nye værdi
-    /// </summary>
-    /// <param name="currencyName">Navnet på den kryptovaluta der angives</param>
-    /// <param name="price">Prisen på en enhed af valutaen målt i dollars. Prisen kan ikke være negativ</param>
     public void SetPricePerUnit(String currencyName, double price) {
+		if(currencyName == "") {
+			throw new ArgumentException("Error: Please enter a currency name.");
+		}
+
 		if(price <= 0) {
-			throw new ArgumentException("Error: Please enter a positive price.");
+			throw new ArgumentException("Error: Please enter a price greater than zero.");
 		}
 
 		currencyPriceList[currencyName] = price;
@@ -22,15 +19,6 @@
 		return currencyPriceList[currencyName];
 	}
 
-    /// <summary>
-    /// Konverterer fra en kryptovaluta til en anden. 
-    /// Hvis en af de angivne valutaer ikke findes, kaster funktionen en ArgumentException
-    /// 
-    /// </summary>
-    /// <param name="fromCurrencyName">Navnet på den valuta, der konverterers fra</param>
-    /// <param name="toCurrencyName">Navnet på den valuta, der konverteres til</param>
-    /// <param name="amount">Beløbet angivet i valutaen angivet i fromCurrencyName</param>
-    /// <returns>Værdien af beløbet i toCurrencyName</returns>
     public double Convert(String fromCurrencyName, String toCurrencyName, double amount) {
 		double fromCurrencyUSDValuePerUnit = GetPricePerUnit(fromCurrencyName);
 		double toCurrencyUSDValuePerUnit = GetPricePerUnit(toCurrencyName);
